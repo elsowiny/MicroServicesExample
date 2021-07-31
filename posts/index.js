70a70f493ed38547c11514aa4cacfca419642cb6
Sherief Elsowiny <elsowiny@yahoo.com>
2021-07-30 23:12:39 -0400

    build[posts] - add express server add express server for creating/getting posts

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id]);
+
+} );
+
+app.listen(4000, () => {
+    console.log('Server is running on 4000');
+} );
